d4f868f9ee0f528f98a2ae8741455eba4aa7a7da
soujanyac <soujanya@yellowmessenger.com>
2023-07-16 23:00:21 +0530

    removed yaml config

--- src/backend/config.js
+++ src/backend/config.js
@@ -1,18 +1,6 @@
-const YAML = require('yaml')
-const fs = require('fs');
 const _ = require('lodash')
 
-const YAML_CONFIG_FILE_PATH = process.env.YAML_CONFIG_FILE_PATH || '../../config.yaml';
 let config = {};
-
-try {
-    const file = fs.readFileSync(YAML_CONFIG_FILE_PATH, 'utf8')
-    config = YAML.parse(file)
-} catch (e) {
-    console.log(e.message, 'error in reading yaml config');
-}
-
-if (!config) config = {};
 
 try {
     _.map(process.env, (value, key) => {
